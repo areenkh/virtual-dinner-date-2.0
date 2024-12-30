9f6a0e02929e197ed0b92303d7fae19fdfcb4906
areenkh <areen.khalaila@gmail.com>
2024-12-30 15:48:43 -0500

    Updates

---
+++ precache-manifest.f62980d84ca70bdb3e20342accac4a02.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c353d1ca1f019a19b8bd67326c245c3",
+    "url": "/virtual-dinner-date-2.0/index.html"
+  },
+  {
+    "revision": "c8cbd4258b4822290cf0",
+    "url": "/virtual-dinner-date-2.0/static/css/main.848a47e8.chunk.css"
+  },
+  {
+    "revision": "ef0d1f2efaa042d00f12",
+    "url": "/virtual-dinner-date-2.0/static/js/2.a84fbe0a.chunk.js"
+  },
+  {
+    "revision": "c8cbd4258b4822290cf0",
+    "url": "/virtual-dinner-date-2.0/static/js/main.7d9b5772.chunk.js"
+  },
+  {
+    "revision": "0a5140d928b30bd65eaf",
+    "url": "/virtual-dinner-date-2.0/static/js/runtime~main.2c0291cc.js"
+  },
+  {
+    "revision": "00b0f4d037300a2ebbb3115cb2818ccd",
+    "url": "/virtual-dinner-date-2.0/static/media/cafe.00b0f4d0.png"
+  },
+  {
+    "revision": "06dce8c9b4eff03d31311e9c56e6b7b3",
+    "url": "/virtual-dinner-date-2.0/static/media/cafe2.06dce8c9.png"
+  },
+  {
+    "revision": "a3d69709008b07c99616d5c78df28bd7",
+    "url": "/virtual-dinner-date-2.0/static/media/catplay.a3d69709.gif"
+  },
+  {
+    "revision": "169b1dc07598d7de23fc016c25b05991",
+    "url": "/virtual-dinner-date-2.0/static/media/chickenburger.169b1dc0.jpg"
+  },
+  {
+    "revision": "66e9fdeca1137fe0d05f3e901e580f2f",
+    "url": "/virtual-dinner-date-2.0/static/media/chickenwings.66e9fdec.jpg"
+  },
+  {
+    "revision": "168cbe0bffa6dccb84ef9f61a58840b0",
+    "url": "/virtual-dinner-date-2.0/static/media/couple1.168cbe0b.png"
+  },
+  {
+    "revision": "4f92aacd82edff8c06fde28bee513105",
+    "url": "/virtual-dinner-date-2.0/static/media/couple2.4f92aacd.png"
+  },
+  {
+    "revision": "5ba4c6a3527aa01e059e113fff393a01",
+    "url": "/virtual-dinner-date-2.0/static/media/couple3.5ba4c6a3.png"
+  },
+  {
+    "revision": "b4094afd956285f3e63256b81c34cc05",
+    "url": "/virtual-dinner-date-2.0/static/media/couple5.b4094afd.png"
+  },
+  {
+    "revision": "664cda09c31836df23ee774267fcb440",
+    "url": "/virtual-dinner-date-2.0/static/media/couple6.664cda09.png"
+  },
+  {
+    "revision": "c63ca0471a25417fc45b44c368507a54",
+    "url": "/virtual-dinner-date-2.0/static/media/couple7.c63ca047.png"
+  },
+  {
+    "revision": "cc566107d19411fd383b001b7030137c",
+    "url": "/virtual-dinner-date-2.0/static/media/couple8.cc566107.png"
+  },
+  {
+    "revision": "d05e8c10853c4e60f167172eb0bd0d32",
+    "url": "/virtual-dinner-date-2.0/static/media/couple9.d05e8c10.png"
+  },
+  {
+    "revision": "9b0351ffc35f317d746a82313ee0b6d6",
+    "url": "/virtual-dinner-date-2.0/static/media/handala.9b0351ff.webp"
+  },
+  {
+    "revision": "4ebae80c917c38480cdfe606b95332c6",
+    "url": "/virtual-dinner-date-2.0/static/media/japan.4ebae80c.png"
+  },
+  {
+    "revision": "a90183ace9988be858e58d9f282593bd",
+    "url": "/virtual-dinner-date-2.0/static/media/middleFinger.a90183ac.png"
+  },
+  {
+    "revision": "f6d8b9d8984c12f7e4382901c6a83381",
+    "url": "/virtual-dinner-date-2.0/static/media/nabil1.f6d8b9d8.png"
+  },
+  {
+    "revision": "45f77f858e38ce6ac63872a0b0ea5639",
+    "url": "/virtual-dinner-date-2.0/static/media/palestineFlag.45f77f85.png"
+  },
+  {
+    "revision": "bf8d2005b7e9b04a3e0176c76b101fb7",
+    "url": "/virtual-dinner-date-2.0/static/media/pizza.bf8d2005.jpg"
+  },
+  {
+    "revision": "33892d44ce52ea7d0a5319a304c2c207",
+    "url": "/virtual-dinner-date-2.0/static/media/shawarma.33892d44.jpeg"
+  },
+  {
+    "revision": "8b1b7cf2872e0bb7ee272eca4c94dbc8",
+    "url": "/virtual-dinner-date-2.0/static/media/tabouleh.8b1b7cf2.jpg"
+  },
+  {
+    "revision": "6315c254150d512c6061fabbfc626463",
+    "url": "/virtual-dinner-date-2.0/static/media/tacos.6315c254.jpg"
+  }
+]);
