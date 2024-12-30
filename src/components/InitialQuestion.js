a40cb44176abb08cec949e8483a7356a9bd0f65e
areenkh <areen.khalaila@gmail.com>
2024-12-30 16:22:16 -0500

    fix centering

--- src/components/InitialQuestion.js
+++ src/components/InitialQuestion.js
@@ -13,7 +13,7 @@ function InitialQuestion({ onAnswer }) {
   const images = [
     { name: 'First picture together', image: firstPic, caption: 'Our first selfie together' },
     { name: 'A special moment', image: secondPic, caption: 'When we had seafood in Sausalito' },
-    { name: 'Another memory', image: thirdPic, caption: 'Your old apartments bathroom' },
+    { name: 'Another memory', image: thirdPic, caption: 'Your old apartment\'s bathroom' },
     { name: 'Fun time', image: fourthPic, caption: 'When we went to Pacifica' },
   ];
 
@@ -23,31 +23,24 @@ function InitialQuestion({ onAnswer }) {
     return <TryAgain onRetry={() => setResponse('ask')} />;
   }
 
-  const handleYes = () => {
-    setResponse('yes');
-  };
-
-  const handleNo = () => {
-    setResponse('no');
-  };
+  const handleYes = () => setResponse('yes');
+  const handleNo = () => setResponse('no');
 
   return (
-    <div className="container">
-      <div className="content">
-        <div className="text-content">
-          <h3>Would you like to go out for a dinner date on our 1 year anniversary?👉🏻👈🏻</h3>
-          <div className="button-group">
-            <button className="button no" onClick={handleNo}>No</button>
-            <button className="button yes" onClick={handleYes}>Yes!</button>
-          </div>
-          <div className="images-row">
-        {images.map((img, index) => (
-          <div key={index} className="image-content">
-            <img src={img.image} alt={img.name} className="styled-image" />
-            <p className="image-caption">{img.caption}</p>
-          </div>
-        ))}
-      </div>
+    <div className="main-container">
+      <div className="content-box">
+        <h3>Would you like to go out for a dinner date on our 1 year anniversary?👉🏻👈🏻</h3>
+        <div className="button-group">
+          <button className="button no" onClick={handleNo}>No</button>
+          <button className="button yes" onClick={handleYes}>Yes!</button>
+        </div>
+        <div className="images-row">
+          {images.map((img, index) => (
+            <div key={index} className="image-content">
+              <img src={img.image} alt={img.name} className="styled-image" />
+              <p className="image-caption">{img.caption}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
